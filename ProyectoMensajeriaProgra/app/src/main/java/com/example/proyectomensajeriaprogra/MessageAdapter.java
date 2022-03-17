b5d180c50b19e73f19c96aab1b4cdfdf18d8e3c4
LuisFernando72 <lpaxelc@miumg.edu.gt>
2022-03-16 21:44:07 -0600

    ProyectoMen44

---
+++ ProyectoMensajeriaProgra/app/src/main/java/com/example/proyectomensajeriaprogra/MessageAdapter.java
@@ -0,0 +1,135 @@
+package com.example.proyectomensajeriaprogra;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+
+    private List<Messages> userMessagesList;
+    private FirebaseAuth mAuth;
+    private DatabaseReference userRef;
+
+    public MessageAdapter(List<Messages> userMessagesList) {
+        this.userMessagesList = userMessagesList;
+    }
+
+
+    public class MessageViewHolder extends RecyclerView.ViewHolder {
+        public TextView senderMessageText, receiverMessageText;
+        public CircleImageView receiverProfileImage;
+        public ImageView messageSenderPicture, messageReceiverPicture;
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            senderMessageText = (TextView) itemView.findViewById(R.id.sender_message_text);
+            receiverMessageText = (TextView) itemView.findViewById(R.id.received_message_text);
+            receiverProfileImage = (CircleImageView) itemView.findViewById(R.id.message_profile_image);
+            messageReceiverPicture=itemView.findViewById(R.id.message_receiver_image_view);
+            messageSenderPicture=itemView.findViewById(R.id.message_sender_image_view);
+        }
+
+    }
+
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.custom_message_layout, viewGroup, false);
+        mAuth = FirebaseAuth.getInstance();
+        return new MessageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        String messageSenderId = mAuth.getCurrentUser().getUid();
+        Messages messages = userMessagesList.get(position);
+
+        String fromUserId = messages.getFrom();
+        String fromMessageType = messages.getType();
+
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(fromUserId);
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.hasChild("image")) {
+                    String receiverImage = snapshot.child("image").getValue().toString();
+                    Picasso.get().load(receiverImage).placeholder(R.drawable.profile_image).into(holder.receiverProfileImage);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+        holder.receiverMessageText.setVisibility(View.GONE);
+        holder.receiverProfileImage.setVisibility(View.GONE);
+        holder.senderMessageText.setVisibility(View.GONE);
+        holder.messageSenderPicture.setVisibility(View.GONE);
+        holder.messageReceiverPicture.setVisibility(View.GONE);
+
+        if (fromMessageType.equals("text")) {
+
+            if (fromUserId.equals(messageSenderId)) {
+                holder.senderMessageText.setVisibility(View.VISIBLE);
+                holder.senderMessageText.setBackgroundResource(R.drawable.sender_messages_layout);
+                holder.senderMessageText.setTextColor(Color.BLACK);
+              //  holder.senderMessageText.setText(messages.getMessage());
+                holder.senderMessageText.setText(messages.getMessage() + "\n \n" + messages.getTime() + " - " + messages.getDate());
+            } else {
+                // holder.senderMessageText.setVisibility(View.INVISIBLE);
+                holder.receiverProfileImage.setVisibility(View.VISIBLE);
+                holder.receiverMessageText.setVisibility(View.VISIBLE);
+                holder.receiverMessageText.setBackgroundResource(R.drawable.receiver_message_layout);
+
+                holder.receiverMessageText.setTextColor(Color.BLACK);
+           //     holder.receiverMessageText.setText(messages.getMessage());
+                holder.receiverMessageText.setText(messages.getMessage() + "\n \n" + messages.getTime() + " - " + messages.getDate());
+
+            }
+        }else if(fromMessageType.equals("image")){
+            if(fromUserId.equals(messageSenderId)){
+                holder.messageReceiverPicture.setVisibility(View.VISIBLE);
+                Picasso.get().load(messages.getMessage()).into(holder.messageReceiverPicture);
+            }else{
+                holder.receiverProfileImage.setVisibility(View.VISIBLE);
+                holder.messageReceiverPicture.setVisibility(View.VISIBLE);
+
+                Picasso.get().load(messages.getMessage()).into(holder.messageReceiverPicture);
+
+            }
+
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return userMessagesList.size();
+    }
+}
+
+
